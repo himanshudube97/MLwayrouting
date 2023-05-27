46837adf621121e03d26ab26d738138b6f8fdccb
himanshudube97 <himanshu.dube13@gmail.com>
2023-05-27 18:22:38 +0530

    second commit used morgan here

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const errorMiddleware = require("./middleware/errorMiddleware");
 
 const app = express();
 app.use(express.json());
+app.use(morgan("tiny"));
 
 const blogRoutes = require("./routes/blogRoutes");
 
